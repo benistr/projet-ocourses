28311416c972a83c92ed82ed3386111416f1a0cb
benistr <benjamin.str@gmail.com>
2019-09-26 18:36:54 +0200

    endpoint login signup routes on going

--- MernApp/controllers/account/lib.js
+++ MernApp/controllers/account/lib.js
@@ -41,34 +41,36 @@ async function signup(req, res) {
 }
 
 async function login(req, res) {
-    const { password, email } = req.body;
-    if (!email || !password) {
-      //Le cas où l'email ou bien le password ne serait pas soumit ou nul
-      return res.status(400).json({
-        text: "Requête invalide"
-      });
-    }
-    try {
-      // On check si l'utilisateur existe en base
-      const findUser = await User.findOne({ email });
-      if (!findUser)
-        return res.status(401).json({
-          text: "L'utilisateur n'existe pas"
-        });
-      if (!findUser.authenticate(password))
-        return res.status(401).json({
-          text: "Mot de passe incorrect"
-        });
-      return res.status(200).json({
-        token: findUser.getToken(),
-        text: "Authentification réussi"
-      });
-    } catch (error) {
-      return res.status(500).json({
-        error
-      });
-    }
+  const { password, email } = req.body;
+  if (!email || !password) {
+    //Le cas où l'email ou bien le password ne serait pas soumit ou nul
+    return res.status(400).json({
+      text: "Requête invalide"
+    });
   }
+  try {
+    // On check si l'utilisateur existe en base
+    const findUser = await User.findOne({ email });
+    if (!findUser)
+      return res.status(401).json({
+        text: "L'utilisateur n'existe pas"
+      });
+    if (!findUser.authenticate(password))
+      return res.status(401).json({
+        text: "Mot de passe incorrect"
+      });
+    return res.status(200).json({
+      token: findUser.getToken(),
+      text: "Authentification réussi"
+    });
+  } catch (error) {
+    return res.status(500).json({
+      error
+    });
+  }
+}
+
+//On exporte nos deux fonctions
 
 exports.login = login;
 exports.signup = signup;
--- MernApp/controllers/userController.js
+++ MernApp/controllers/userController.js
@@ -1,6 +1,6 @@
-    const account = require('./account/lib.js');
+const account = require('./account/lib.js');
 
-    module.exports = function (app) {
-        app.post('/login',account.login);
-        app.post('/signup',account.signup);
-    }
+module.exports = function (app) {
+    app.post('/login',account.login);
+    app.post('/signup',account.signup);
+}
