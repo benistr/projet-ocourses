0b50eab9d02e70590befcf77828988940c9c9e6d
nairoprod <dorianricordel@yahoo.fr>
2019-09-27 16:29:06 +0200

    header responsive en cours

--- MernApp/client/src/components/Header/index.js
+++ MernApp/client/src/components/Header/index.js
@@ -6,7 +6,7 @@ import { Header, Segment } from 'semantic-ui-react'
 <link rel="stylesheet" type="text/css" href="semantic/dist/semantic.min.css"></link>
 
 // Styles et assets
-import './header.sass';
+import './header.scss';
 import Logo from './images/logo.png';
 import Menu from './images/menu.png';
 import Compte from './images/compte.png';
@@ -14,6 +14,15 @@ import Compte from './images/compte.png';
 const Nav = () => (
   <div id="header">
     <div><img className="logo" src={Logo}/></div>
+      <div>
+        <ul id="nav">
+          <li id="nav">Mes listes</li>
+          <li id="nav">Mes recettes</li>
+          <li id="nav">Légumes de saison</li>
+          <li>Qui Sommes-nous?</li>
+          <li>Mon Compte</li>
+        </ul>
+      </div>
     <div><img className="compte" src={Compte} /></div>
     <div><img className="menu" src={Menu}/></div>
   </div>
