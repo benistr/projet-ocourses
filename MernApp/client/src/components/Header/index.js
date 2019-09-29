90e3e83f7513a37987fde459a3031e3181c26f35
nairoprod <dorianricordel@yahoo.fr>
2019-09-29 16:23:07 +0200

    header responsive ok

--- MernApp/client/src/components/Header/index.js
+++ MernApp/client/src/components/Header/index.js
@@ -13,18 +13,35 @@ import Compte from './images/compte.png';
 
 const Nav = () => (
   <div id="header">
-    <div><img className="logo" src={Logo}/></div>
-      <div>
-        <ul id="nav">
-          <li id="nav">Mes listes</li>
-          <li id="nav">Mes recettes</li>
-          <li id="nav">Légumes de saison</li>
-          <li>Qui Sommes-nous?</li>
-          <li>Mon Compte</li>
-        </ul>
-      </div>
-    <div><img className="compte" src={Compte} /></div>
-    <div><img className="menu" src={Menu}/></div>
+    
+      <ul id="menu">
+        <li className="logo-div">
+          <a id="a" href=""><img className="logo" src={Logo}/></a>
+        </li>
+        <li id="nav">
+          <a id="a" href="">Mes Listes</a>
+        </li>
+        <li id="nav">
+          <a id="a" href="">Mes Recettes</a>
+        </li>
+        <li id="nav">
+          <a id="a" href="">Légumes de saison</a>
+        </li>
+        <li id="nav">
+          <a id="a" href="">Qui Sommes-nous?</a>
+        </li>
+        <li id="nav">
+          <a id="a" href="">Mon Compte</a>
+        </li>
+        <li id="picto">
+          <a id="a" href=""><img className="picto-compte" src={Compte} /></a>
+          <a id="a" href=""><img className="picto-menu" src={Menu}/></a>
+          <li id="nav2">
+          <a id="a" href="">Connexion</a>
+        </li>
+        </li>
+        
+      </ul>
   </div>
 )
 
