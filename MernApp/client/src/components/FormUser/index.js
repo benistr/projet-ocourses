ea7a66f29b968f160b85760395942819eb963a6a
mitaksim <miriamt.simonnet@gmail.com>
2019-10-01 15:06:19 +0200

    Form design ok

--- MernApp/client/src/components/FormUser/index.js
+++ MernApp/client/src/components/FormUser/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import { Button, Form} from 'semantic-ui-react'
 
+import './style.scss';
+
+/**
+* Local import
+*/
+import Logo from './logo.png';
+
 
 class FormUser extends React.Component {
     continue = event => {
@@ -11,21 +18,26 @@ class FormUser extends React.Component {
         // On récupères les props values de userAccount en les déstructurant
         const { values, handleChange } = this.props;
         return (
-            <div classname="userForm">
+            <div className="userForm">
+
+                <h1 className="form-text">Créez votre compte</h1> 
+                <img className="form-logo" src={Logo}/>
+                
+    
             <Form className="form">
             
-                <Form.Field>
-                  <label>First Name</label>
+                <Form.Field className="input-style">
+                  <label className="input-label" >Votre prénom</label>
                   <input placeholder='First Name' />
                 </Form.Field>
 
-                <Form.Field>
-                  <label>Last Name</label>
+                <Form.Field className="input-style">
+                  <label className="input-label">Votre nom</label>
                   <input placeholder='Last Name' />
                 </Form.Field>
 
-                <Form.Field>
-                  <label>Email</label>
+                <Form.Field className="input-style">
+                  <label className="input-label">Votre email</label>
                   <input placeholder='Email' />
                 </Form.Field>
 
