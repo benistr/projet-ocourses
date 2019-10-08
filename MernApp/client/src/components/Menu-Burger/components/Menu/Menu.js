5d04535f8b374893c48fac4e45e7f76cd8f1fb56
Kévin Bégasse <kevinbegasse@gmail.com>
2019-10-08 14:34:34 +0200

    menu closes when link cliked

--- MernApp/client/src/components/Menu-Burger/components/Menu/Menu.js
+++ MernApp/client/src/components/Menu-Burger/components/Menu/Menu.js
@@ -7,7 +7,7 @@ import Logo from './logo.png';
 import Fleche from './fleche.png';
 
 const Menu = ({ open, ...props }) => {
-  
+  console.log(props);
   const isHidden = open ? true : false;
   const tabIndex = isHidden ? 0 : -1;
 
@@ -23,7 +23,7 @@ const Menu = ({ open, ...props }) => {
 
 
       <NavLink to='/listes' id="dist" href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="txt">
             Mes Listes<img className="fleche" src={Fleche}/>
@@ -32,7 +32,7 @@ const Menu = ({ open, ...props }) => {
       </NavLink>
 
       <NavLink to="/recettes" href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
               Mes recettes<img className="fleche" src={Fleche}/>
@@ -41,7 +41,7 @@ const Menu = ({ open, ...props }) => {
         </NavLink>
 
         <NavLink to="/saisons" href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
             Légumes de saison<img className="fleche" src={Fleche}/>
@@ -50,7 +50,7 @@ const Menu = ({ open, ...props }) => {
         </NavLink>
 
         <NavLink to='/team'href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
             Qui sommes-nous?<img className="fleche" src={Fleche}/>
@@ -59,7 +59,7 @@ const Menu = ({ open, ...props }) => {
         </NavLink>
 
         <NavLink to='/account' href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
             Mon Compte<img className="fleche" src={Fleche}/>
@@ -70,7 +70,7 @@ const Menu = ({ open, ...props }) => {
         <div className="space"></div>
 
         <NavLink to='' href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
             Contact<img className="fleche" src={Fleche}/>
@@ -79,7 +79,7 @@ const Menu = ({ open, ...props }) => {
         </NavLink>
 
         <NavLink to='' href="">
-        <a className="bord" href="/" tabIndex={tabIndex}>
+        <a className="bord" href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text">
             Mentions légales<img className="fleche" src={Fleche}/>
@@ -88,7 +88,7 @@ const Menu = ({ open, ...props }) => {
         </NavLink>
 
         <NavLink to='' href="">
-        <a href="/" tabIndex={tabIndex}>
+        <a href="/" tabIndex={tabIndex} onClick={() => props.setOpen(false)}>
           <span aria-hidden="true"></span>
           <div className="text" id="space">
             CGU<img className="fleche" src={Fleche}/>
