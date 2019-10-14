4c4a86fef23e3266a682955cf2d221141bf148de
nairoprod <dorianricordel@yahoo.fr>
2019-10-14 14:00:09 +0200

    accroche fixed

--- MernApp/client/src/components/Main/index.js
+++ MernApp/client/src/components/Main/index.js
@@ -1,48 +1,18 @@
 import React from 'react';
-import { Responsive } from 'semantic-ui-react';
+import { Responsive } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import * as jwtDecode from 'jwt-decode';
 import axios from 'axios';
 
-import Fred from './fred2.png';
-import Jamy from './jamy1.png';
-
 //Local imports
 import './styles.scss';
 
-
-const Main = () => {
-    
-        return <div className="mainContainer">
-            <p className="navigation">▶ Accueil</p>
-                <div className="gradient-box">
-                    <div className="topSpeech">
-                    
-                        <h2 className="slogan">On ne peut pas pousser pas votre caddie 
-                        mais on peut vous aider pour le reste !</h2>
-                        <div className="description">
-                            <div className="fred">
-                                <img className="fred-img" src={Fred}/>
-                                <div className="fred-text">Mais alors dis-moi Jamy, qu'est-ce que c'est O'Courses ?</div>
-                            </div>
-                        
-                            <div className="jamy">
-                                <div className="jamy-text">O'Courses, c'est l'application qui va te faciliter la vie 
-                                en te permettant de créer et gérer tes listes de courses. 
-                                Parce que moins de temps à faire les courses,
-                                c'est plus de temps pour manger !
-                                </div>
-                                <img className="jamy-img" src={Jamy}/>
-                                
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
+import Fred from './fred2.png';
+import Jamy from './jamy1.png';
 
 class Main extends React.Component {
-    constructor(props) {
+    constructor(props) {
         super(props);
         this.state = {
             isConnected: false,
@@ -84,22 +54,30 @@ componentDidUpdate(){
             <p>Bonjour {this.state.name} !
             </p>
         }
-            <div className="topSpeech"><p className="description"><h2 className="slogan">On ne poussera pas votre caddie, mais on vous aide pour le reste !</h2>
-            <br></br><br></br>Mais alors dis-moi Jamy, qu'est-ce que c'est O'Courses ? 
-            <br></br><br></br>O'Courses, c'est l'application qui va vous faciliter la vie en vous permettant de créer et gérer vos listes de courses. Vous passerez moins de temps à faire les courses, et plus de temps à manger !</p>
-            </div>
-
-                    <div className="mainContent"><NavLink to="/saisons">
-                        <div className="title">Octobre</div>
-                        <div className="legumes"> 
-                            <div className="summary">
-                            <p className="summary-title">Découvrez les fruits & légumes du mois</p>
-                            <Responsive minWidth={1024}>
-                            </Responsive>
-                            </div>
-                        </div>
-                        </NavLink>
+        <div className="gradient-box">
+        <div className="topSpeech">
+                    
+            <h2 className="slogan">On ne peut pas pousser pas votre caddie 
+            mais on peut vous aider pour le reste !</h2>
+            <div className="description">
+                <div className="fred">
+                    <img className="fred-img" src={Fred}/>
+                    <div className="fred-text">Mais alors dis-moi Jamy, qu'est-ce que c'est O'Courses ?</div>
+                </div>
+            
+                <div className="jamy">
+                    <div className="jamy-text">O'Courses, c'est l'application qui va te faciliter la vie 
+                    en te permettant de créer et gérer tes listes de courses. 
+                    Parce que moins de temps à faire les courses,
+                    c'est plus de temps pour manger !
                     </div>
+                    <img className="jamy-img" src={Jamy}/>
+                    
+                    </div>
+                </div>
+            </div>
+        </div>
+
 
                 <div className="mainContent"><a href="">
                 <div className="title">Lasagnes à l'italienne</div>
@@ -118,7 +96,6 @@ componentDidUpdate(){
                     </div>
                     </a>
                 </div>
-
                 <div className="mainContent"><a href="">
                     <div className="title">Crêpes au suc'</div>
                     <div className="crepes"> 
@@ -136,7 +113,6 @@ componentDidUpdate(){
                     </div>
                     </a>
                 </div>
-
                 <div className="mainContent"><a href="">
                     <div className="title">Pizza maison</div>
                     <div className="pizza"> 
@@ -154,13 +130,28 @@ componentDidUpdate(){
                     </div>
                     </a>
                 </div>
-
+                <div className="mainContent"><a href="">
+                    <div className="title">Pizza maison</div>
+                    <div className="pizza"> 
+                        <div className="summary">
+                        <Responsive minWidth={1024}>
+                        <ul className="recipe-items">
+                            <li>- Mortadelle</li>
+                            <li>- 1 poivron</li>
+                            <li>- 3 champignons</li>
+                            <li>- Mozzarella</li>
+                            
+                        </ul>
+                        </Responsive>
+                        </div>    
+                    </div>
+                    </a>
+                </div>
             </div>
         )
             
 };
 }
-
 // Étape 1 : on définit des stratégies de connexion au store de l'app.
 const connectionStrategies = connect(
     // 1er argument : stratégie de lecture (dans le state privé global)
