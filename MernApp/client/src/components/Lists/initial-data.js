08fe74db04cfc73e4fe3020921822205baab2d27
Kévin Bégasse <kevinbegasse@gmail.com>
2019-10-17 14:40:22 +0200

    standard name for list page without list saved

--- MernApp/client/src/components/Lists/initial-data.js
+++ MernApp/client/src/components/Lists/initial-data.js
@@ -2,17 +2,17 @@
 const initialData = {
   tasks: {
     'task-1': { id: 'task-1', content: '- Cette liste est vide pour le moment!'},
-    'task-2': { id: 'task-2', content: 'Pouet!'},
+    'task-2': { id: 'task-2', content: "C'est l'occasion pour vous d'aller en créer une :) !"},
   },
   columns: {
     'column-1': {
       id: 'column-1',
-      title: 'Carrefour',
+      title: 'Ca donne la pêche !',
       taskIds: ['task-1', 'task-2'],
     },
     'column-2': {
       id: 'column-2',
-      title: 'Leclerc',
+      title: 'Vous pourrez même indiquer le titre que vous voulez ici! ',
       taskIds: [],
     },
   },
