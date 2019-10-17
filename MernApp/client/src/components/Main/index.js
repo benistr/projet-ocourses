c6ab1ae808e3897f2bef2aaff6fca0e27bf35178
benistr <benjamin.str@gmail.com>
2019-10-17 15:45:10 +0200

    minor corrections on recipe

--- MernApp/client/src/components/Main/index.js
+++ MernApp/client/src/components/Main/index.js
@@ -89,53 +89,51 @@ class Main extends React.Component {
                             </Responsive>
                     </NavLink>
                 </div>
+
                 <div className="mainContent"><a href="">
                 <div className="title">Lasagnes à l'italienne</div>
                 <div className="lasagnes"> 
                     <div className="summary">
-                    <Responsive minWidth={1024}>
                         <ul className="recipe-items">
                             <li>- 1 Carotte </li>
                             <li>- 1 Branche de céleri </li>
                             <li>- 800gr hâché mélangé </li>
                             <li>- Sauce tomate </li>
-                            
+                            <li><a>Voir plus</a></li>
                         </ul>
-                        </Responsive>
                         </div>    
                     </div>
                     </a>
                 </div>
+
                 <div className="mainContent"><a href="">
                     <div className="title">Crêpes au suc'</div>
                     <div className="crepes"> 
                         <div className="summary">
-                        <Responsive minWidth={1024}>
                         <ul className="recipe-items">
                             <li>- 300gr de farine</li>
                             <li>- 3 oeufs</li>
                             <li>- 50gr de beurre fondu</li>
                             <li>- 60cl de lait</li>
                             <li>- Du suc'</li>
+                            <li><a>Voir plus</a></li>
                         </ul>
-                        </Responsive>
                         </div>    
                     </div>
                     </a>
                 </div>
+
                 <div className="mainContent"><a href="">
                     <div className="title">Pizza maison</div>
                     <div className="pizza"> 
                         <div className="summary">
-                        <Responsive minWidth={1024}>
                         <ul className="recipe-items">
                             <li>- Mortadelle</li>
                             <li>- 1 poivron</li>
                             <li>- 3 champignons</li>
                             <li>- Mozzarella</li>
-                            
+                            <li><a>Voir plus</a></li>
                         </ul>
-                        </Responsive>
                         </div>    
                     </div>
                     </a>
