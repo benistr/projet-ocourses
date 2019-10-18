daaee652895c99d9de2c3d2fd92112b50c017714
benistr <benjamin.str@gmail.com>
2019-10-18 02:20:03 +0200

    new carousel

--- MernApp/client/src/components/Carousel/index.js
+++ MernApp/client/src/components/Carousel/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react';
-import AliceCarousel from 'react-alice-carousel';
-import "react-alice-carousel/lib/alice-carousel.css";
+import { Slide } from 'react-slideshow-image';
+import './carousel.sass';
 
 import Janvier from '../Season/Janvier.jpg';
 import Février from '../Season/Février.jpg';
@@ -13,26 +13,73 @@ import Juillet from '../Season/Juillet.jpg';
 import Aout from '../Season/Aout.jpg';
 import Septembre from '../Season/Septembre.jpg';
 import Octobre from '../Season/Octobre.jpg';
-
-import './carousel.scss'
  
+const proprietes = {
+  duration: 100000000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+}
+
 const Carousel = () => {
 
-  const handleOnDragStart = e => e.preventDefault()
 
   return (
-    <AliceCarousel mouseDragEnabled >
-      <img src={Janvier} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Février} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Mars} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Avril} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Mai} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Juin} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Juillet} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Aout} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Septembre} onDragStart={handleOnDragStart} className="yours-custom-class" />
-      <img src={Octobre} onDragStart={handleOnDragStart} className="yours-custom-class" />
-    </AliceCarousel>
+    <div className="containerSlide">
+      <Slide {...proprietes}>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Janvier} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Février} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Mars} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Avril} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Mai} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Juin} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Juillet} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Aout} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Septembre} />
+          </div>        
+        </div>
+        <div className="each-slide">
+          <div>
+            <img className="img-carousel" src={Octobre} />
+          </div>        
+        </div>
+      </Slide>
+    </div>
   )
 }
 
